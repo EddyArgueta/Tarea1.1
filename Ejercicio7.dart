d2bb45b4cd15c56389bcc245f0f7916d7f4c33d0
CarlosArgueta <carguetar@unah.hn>
2024-10-02 14:32:43 -0600

    Ejercicio 7

---
+++ Ejercicio7.dart
@@ -0,0 +1,15 @@
+//Crea un programa que encuentre el número más pequeño en una lista, sin
+//utilizar una función predeterminada por Dart.
+
+void main() {
+  List<int> lista = [23, 11, 77, 8, 49, 80, 21];
+  int minimo = lista[0];
+
+  for (int i = 1; i < lista.length; i++) {
+    if (lista[i] < minimo) {
+      minimo = lista[i];
+    }
+  }
+
+  print('El numero más pequeño es: $minimo');
+}
